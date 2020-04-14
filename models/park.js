c51f94bf128f659e25510d886276864fb99f8384
Tony Tang <teokigi@hotmail.com>
2020-04-14 19:25:10 +0100

    opening commit

---
+++ models/park.js
@@ -0,0 +1,63 @@
+const Dinosaur = require('./dinosaur.js')
+
+const Park = function (name, ticketprice, dinosaurs) {
+  this.name = name;
+  this.ticketprice = ticketprice;
+  this.dinosaurs = dinosaurs;
+}
+// Add a dinosaur to its collection of dinosaurs
+Park.prototype.addDinosaur = function(new_dinosaur){
+	this.dinosaurs.push(new_dinosaur);
+}
+// Remove a dinosaur from its collection of dinosaurs
+Park.prototype.removeDinosaur = function(){
+	this.dinsoaurs.pop();
+}
+// Find the dinosaur that attracts the most visitors
+Park.prototpye.mostPopular = function(){
+//get all dinosaurs data, compare the gusts/day and list one with top.
+	let most_popular;
+	for (dinosaur of this.dinosaurs){
+		if (!most_popular){
+			most_popular = dinosaur;
+			}
+		else{
+			if (most_popular.guestsAttractedPerDay() > dinosaur.guestsAttractedPerDay()){
+			most_popular = dinosaur
+			}
+		}
+	}
+	return most_popular
+}
+// Find all dinosaurs of a particular species
+Park.prototype.matchesSpecies = function(species){
+	let speciesMatches = [];
+	for (dinosaur of this.dinosaurs){
+		if (dinosaur.species == species){
+			speciesMatches.push(dinosaur)
+		}
+	}
+	return speciesMatches
+}
+// Calculate the total number of visitors per day
+Park.prototype.vistorsPerDay = function(){
+	let total = 0;
+	for (dinosaur of this.dinosaurs){
+		total += dinosaur.guestsAttractedPerDay
+	}
+	return total;
+}
+// Calculate the total number of visitors per year
+Park.prototype.visitorsPerYear = function(){
+
+	const totalPerDay = this.visitorsPerDay();
+	const totalPerYear = totalPerDay*365;
+	return totalPerYear
+}
+// Calculate the total revenue from ticket sales for one year
+Park.prototype.annualSales = function(){
+	const annualVisits = this.visitorsPerYear();
+	const salesTotal = annualVists*this.ticketPrice;
+	return salesTotal;
+}
+module.exports = Park;
---
+++ specs/park_spec.js
@@ -0,0 +1,59 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+
+  beforeEach(function () {
+        dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50);
+        dinosaur2 = new Dinosaur('Raptor','carnivore',75);
+        dinosaur3 = new Dinosaur('triceratops','herbivore',125);
+        dinosaur4 = new Dinosaur('Stegosaurus','herbivore',15);
+        dinosaurs = [dinosaur1,dinosaur2,dinosaur3]
+        jurassicPark = new Park('Jurassic Park',100,dinosaurs);
+  })
+
+  it('001 - should have a name',function(){
+    assert.equal('Jurassic Park',jurassicPark.name);
+    });
+//
+//   it('002 - should have a ticket price',function(){
+//     assert.equal(100,jurassicPark.ticketPrice);
+//     });
+//
+//   it('003 - should have a collection of dinosaurs',function(){
+//     assert.deepStrictEqual([dinosaur1,dinosaur2,dinosaur3],JurassicPark.dinosaurs);
+//     });
+//
+//   it('004 - should be able to add a dinosaur to its collection',function(){
+//     jurassicPark.addDinosaur(dinosaur4);
+//     assert.equal(4,jurassicPark.dinosaurs.length);
+// });
+//
+//   it('005 - should be able to remove a dinosaur from its collection',function(){
+//     jurassicPark.removeDinosaur();
+//     assert.equal(2,jurassicPark.dinosaurs.length);
+// });
+//
+//   it('006 - should be able to find the dinosaur that attracts the most visitors',function(){
+//     const starAttraction = jurassicPark.mostPopular();
+//     assert.equal('triceratops',starAttraction.species);
+// });
+//
+//   it('007 - should be able to find all dinosaurs of a particular species',function(){
+//     const matchingSpecies = jurassicPark.matchesSpecies('Raptor');
+//     assert.equal(1,matchingSpecies.length);
+// });
+//
+//   it('008 - should be able to calculate the total number of visitors per day',function(){
+//     // 50 + 75 = 125 + 125 = 250 + 15 = 265
+//     assert.equal(265,jurassicPark.visitorsPerDay());
+// });
+//
+//   it('009 - should be able to calculate the total number of visitors per year',function(){
+//     assert.equal(265*365,jurassicPark.visitorsPerYear());
+// });
+//
+//   it('010 - should be able to calculate total revenue for one year');
+//     assert.equal(265*365*100,jurassicPark.annualSales());
+// });
